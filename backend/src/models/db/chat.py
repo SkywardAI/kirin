5600be0932542a8b95437cf57c0da83c90e246e8
micost <chuan.z@hotmail.com>
2024-03-17 20:27:00 +0800

    feat:v0.1 update api
    Signed-off-by: micost <chuan.z@hotmail.com>

---
+++ backend/src/models/db/chat.py
@@ -0,0 +1,31 @@
+import datetime
+
+import sqlalchemy
+from sqlalchemy.orm import Mapped as SQLAlchemyMapped, mapped_column as sqlalchemy_mapped_column
+from sqlalchemy.sql import functions as sqlalchemy_functions
+
+from src.repository.table import Base
+
+
+class Session(Base):  # type: ignore
+    __tablename__ = "session"
+
+    id: SQLAlchemyMapped[int] = sqlalchemy_mapped_column(primary_key=True, autoincrement="auto")
+    account_id: SQLAlchemyMapped[int] = sqlalchemy_mapped_column(nullable=True)
+    name: SQLAlchemyMapped[str] = sqlalchemy_mapped_column(sqlalchemy.String(length=64), nullable=True)
+
+    __mapper_args__ = {"eager_defaults": True}
+
+
+class ChatHistory(Base):  # type: ignore
+    __tablename__ = "chat_history"
+
+    id: SQLAlchemyMapped[int] = sqlalchemy_mapped_column(primary_key=True, autoincrement="auto")
+    session_id: SQLAlchemyMapped[int] = sqlalchemy_mapped_column(nullable=False)
+    is_bot_msg: SQLAlchemyMapped[bool] = sqlalchemy_mapped_column(sqlalchemy.Boolean, nullable=False, default=False)
+    message: SQLAlchemyMapped[str] = sqlalchemy_mapped_column(sqlalchemy.String(length=4096), nullable=False)
+    created_at: SQLAlchemyMapped[datetime.datetime] = sqlalchemy_mapped_column(
+        sqlalchemy.DateTime(timezone=True), nullable=False, server_default=sqlalchemy_functions.now()
+    )
+
+    __mapper_args__ = {"eager_defaults": True}
--- backend/src/models/schemas/chat.py
+++ backend/src/models/schemas/chat.py
@@ -6,10 +6,22 @@ from src.models.schemas.base import BaseSchemaModel
 
 
 class ChatInMessage(BaseSchemaModel):
-    sessionId: Optional[str] = Field(None)
+    accountID: Optional[int] = Field(None)
+    sessionId: Optional[int] = Field(None)
     message: str
 
 
 class ChatInResponse(BaseSchemaModel):
-    sessionId: str
+    sessionId: int
+    message: str
+
+
+class Session(BaseSchemaModel):
+    id: int
+    name: str | None
+
+
+class ChatHistory(BaseSchemaModel):
+    id: int
+    type: str
     message: str
