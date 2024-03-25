3e34132de07f782884acb6d9c390c5521be1f9e5
Aisuko <urakiny@gmail.com>
2024-03-25 17:24:24 +1100

    remove ml libraries
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- backend/src/repository/rag/chat.py
+++ backend/src/repository/rag/chat.py
@@ -1,27 +1,16 @@
 import csv
 
-import torch
-from sentence_transformers import SentenceTransformer
-from sentence_transformers.util import cos_sim
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
-
-
 from src.config.settings.const import CHAT_COMTEXT, DEFAULT_MODEL, MAX_SQL_LENGTH, UPLOAD_FILE_PATH
 from src.repository.rag.base import BaseRAGRepository
 from src.utilities.devices.devices import get_device
 
 
 class RAGChatModelRepository(BaseRAGRepository):
-    model_name = "deepset/roberta-base-squad2"
-
-    nlp = pipeline("question-answering", model=model_name, tokenizer=model_name)
 
     async def load_model(self, session_id: int, model_name: str) -> bool:
         # Init model with input model_name
         try:
-            # https://github.com/UKPLab/sentence-transformers/blob/85810ead37d02ef706da39e4a1757702d1b9f7c5/sentence_transformers/SentenceTransformer.py#L47
-            model = SentenceTransformer(model_name, device=get_device())
-            model.max_seq_length = MAX_SQL_LENGTH
+            pass
         except Exception as e:
             print(e)
             return False
@@ -34,9 +23,7 @@ class RAGChatModelRepository(BaseRAGRepository):
         # cos_scores = cos_sim(query_embedding, self.embeddings)[0]
         # top_results = torch.topk(cos_scores, k=1)
         # response_msg = self.data[top_results[1].item()]
-        QA_input = {"question": input_msg, "context": CHAT_COMTEXT}
-        res = self.nlp(QA_input)
-        return res["answer"]
+        return "response message"
 
     async def load_csv_file(self, file_name: str, model_name: str) -> bool:
         # read file named file_name and convert the content into a list of strings @Aisuko
@@ -54,7 +41,6 @@ class RAGChatModelRepository(BaseRAGRepository):
                 # Add the row to the list
                 self.data.extend(row)
         print(self.data)
-        self.model = SentenceTransformer(model_name, "cuda")
         row_embedding = self.model.encode(self.data, convert_to_tensor=True).to("cuda")
         # TODO
         self.embeddings.append(row_embedding)
