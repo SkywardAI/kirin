9e3078b1d29a05c2ce4bd865a838eb217a6a8bce
Rob <chuan.z@hotmail.com>
2024-04-08 15:33:15 +0800

    fix:fix the validation error of external confi not allowed
    Signed-off-by: Rob <chuan.z@hotmail.com>

--- backend/src/config/settings/development.py
+++ backend/src/config/settings/development.py
@@ -6,3 +6,16 @@ class BackendDevSettings(BackendBaseSettings):
     DESCRIPTION: str | None = "Development Environment."
     DEBUG: bool = True
     ENVIRONMENT: Environment = Environment.DEVELOPMENT
+    BACKEND_SERVER_HOST: str
+    BACKEND_SERVER_PORT: int
+    BACKEND_SERVER_WORKERS: int
+    POSTGRES_DB: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_PORT: int
+    POSTGRES_SCHEMA: str
+    POSTGRES_USERNAME: str
+    POSTGRES_HOST: str
+    POSTGRES_URI: str
+    ETCD_AUTO_COMPACTION_MODE: str
+    ETCD_AUTO_COMPACTION_RETENTION: str
+    ETCD_QUOTA_BACKEND_BYTES: str
