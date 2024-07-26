fe75df670397d2de63cd0e294aa2b2d581ee4539
Aisuko <urakiny@gmail.com>
2024-07-26 11:26:48 +1000

    fix db async session in process issue (#323)
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- backend/src/api/dependencies/session.py
+++ backend/src/api/dependencies/session.py
@@ -8,9 +8,13 @@ from src.repository.database import async_db
 async def get_async_session() -> typing.AsyncGenerator[SQLAlchemyAsyncSession, None]:
     try:
         yield async_db.async_session
+        loguru.logger.info(f"Async pool:{async_db.pool.status()}")
     except Exception as e:
         loguru.logger.info(f"Exception --- {e}")
         await async_db.async_session.rollback()
         raise
+    else:
+        # https://github.com/grillazz/fastapi-sqlalchemy-asyncpg/issues/63
+        await async_db.async_session.commit()
     finally:
         await async_db.async_session.close()
